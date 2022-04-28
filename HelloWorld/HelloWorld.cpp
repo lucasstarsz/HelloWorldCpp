adac8c4f4a26988b59a2920a798a97afc99ce99c
lucasstarsz <andrewrcdey@gmail.com>
2022-04-28 16:18:43 -0400

    playing around with C headers, my first commit from an IDE

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,20 +1,14 @@
-// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// HelloWorld.cpp : My attempt at learning C++ from the very beginning.
 
 #include <iostream>
+#include <stdio.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	printf("C makes an appearance!\nEnter a character. ");
+
+	int letter = getchar();
+	std::cout << (char) letter << " was printed." << std::endl;
+
+	return EXIT_SUCCESS;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
